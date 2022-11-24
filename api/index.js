84992d418142221d0c74156707a3e799d5774a63
FedeHuertas <federicohuertas@outlook.com>
2022-11-23 23:58:28 -0300

    Puerto como variable de entorno

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(5432, () => {
-    console.log('%s listening at 5432'); // eslint-disable-line no-console
+  server.listen(process.env.port, () => {
+    console.log(`%s listening at ${process.env.port}`); // eslint-disable-line no-console
   });
 });
