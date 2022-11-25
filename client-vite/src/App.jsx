6e61cc8eddf362ca4151d8675c999424221a26d3
FedeHuertas <federicohuertas@outlook.com>
2022-11-24 21:23:27 -0300

    Fix: import Home page

--- client-vite/src/App.jsx
+++ client-vite/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import CreateRecipe from './pages/CreateRecipe';
 import RecipeDetail from './components/RecipeDetail';
 import Landing from './components/Landing';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 function App() {
   return (
