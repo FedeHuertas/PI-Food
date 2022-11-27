a7073561332fd202a7d6a9f501ef136841e932b0
FedeHuertas <federicohuertas@outlook.com>
2022-11-27 15:58:39 -0300

    Cambios css
    - Bontones cursor pointer
    - Estilos landing

--- client-vite/src/components/Landing.jsx
+++ client-vite/src/components/Landing.jsx
@@ -4,10 +4,10 @@ import s from './Landing.module.css'
 
 const Landing = () => {
     return (
-        <div>
+        <div className={s.container} >
             <h1 className={s.text} >Bienvenido a mi PI</h1>
             <h1 className={s.text} >Henry-Food</h1>
-            <h4>Presiona el botón para empezar...</h4>
+            <h4 className={s.text} >Presiona el botón para empezar...</h4>
             <Link to='/home'>
                 < button className={s.startButton} >¡Vamos!</button>
             </Link> 
